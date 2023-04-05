ab76a68bf2b76de10272c628906beb6df94abd7b
Zaur <zaur.cherchesov@mail.ru>
2023-04-05 21:45:33 +0300

    add Example Base

---
+++ Example06_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int A = 1;
+int B = 5;
+int C = 8;
+int D = 3;
+
+int max = A;
+
+if (A > max)    max = A;
+if (B > max)    max = B;
+if (C > max)    max = C;
+if (D > max)    max = D;
+
+Console.Write(max);
