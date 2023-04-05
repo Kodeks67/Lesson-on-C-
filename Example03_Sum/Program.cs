81ca390123c18e8634db3fd960da2f24dd4c2763
Zaur <zaur.cherchesov@mail.ru>
2023-04-05 21:25:06 +0300

    add in Example Sum:Random Sum

--- Example03_Sum/Program.cs
+++ Example03_Sum/Program.cs
@@ -2,3 +2,12 @@
 int numberB = 7;
 int sum = numberA + numberB;
 Console.WriteLine(sum);
+
+// Random sum
+
+int numberD = new Random().Next(1,100);
+Console.WriteLine(numberD);
+int numberC = new Random().Next(1,100);
+Console.WriteLine(numberC);
+int sum_ran = numberD + numberC;
+Console.WriteLine(sum_ran);
