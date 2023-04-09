816409b4fc0644a173a1a6957b46d24f99756130
Zaur <zaur.cherchesov@mail.ru>
2023-04-09 13:00:46 +0300

    add Example Max method

---
+++ Example/Example08_IntroMax/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a = 3;
+int b = 4;
+int c = 213;
+
+int max = Max(a, b, c);
+
+Console.WriteLine(max);
