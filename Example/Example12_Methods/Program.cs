a9b4a2e11809ddb81b18361eb895e4a401c51698
Zaur <zaur.cherchesov@mail.ru>
2023-04-16 13:42:18 +0300

    added folder Example Methods functions

---
+++ Example/Example12_Methods/Program.cs
@@ -0,0 +1,142 @@
+﻿// 1 Вид
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+// 2 Вид
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Nice car");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Nice car Red", 5); // можно писать так 
+Method21(msg: "Nice car Red", count: 5); // лучше писать так для понятия
+
+
+// 3 Вид
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+// 4 Вид
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "qwer");
+Console.WriteLine(res);
+
+// 4.1 Вид с циклом for
+
+string Method41(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res2 = Method41(10, "qwer2.1");
+Console.WriteLine(res2);
+
+// 5 Вид цикл в цикле
+
+for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j < 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// 6 Task Замена пробелов на черточки, букву 'к' заменить на 'К', а букву 'С' заменить на 'c'.
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+
+
+// 7 Вид массива 
+
+int[] arr = { 1, 5, 4, 1, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
